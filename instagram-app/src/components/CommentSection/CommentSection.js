c5c43978616ea04a3a12de86d741e9fe766abe5a
justiny2c <justiny2c@gmail.com>
2019-06-03 15:25:37 -0700

    WIP: Working on imageUrl

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -1,15 +1,28 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function CommentSection(props){
     console.log(props)
     return (
-        props.postComments.map(comment => (
+        <>
+        {props.postComments.map(comment => (
             
             <div className = "comment">
-                <p>{comment.comment}</p>
+                <p>{comment.username}</p>
+                <p>{comment.text}</p>
             </div>
-        ))
+        ))}
+        </>
     )
 }
+
+// CommentSection.propTypes = {
+//     comment: PropTypes.arrayOf(
+//         PropTypes.shape({
+//             id: PropTypes.number.isRequired,
+//             username: PropTypes.string.isRequired,
+//         })
+//     )
+// }
 
 export default CommentSection;
--- instagram-app/src/components/PostContainer/PostContainer.js
+++ instagram-app/src/components/PostContainer/PostContainer.js
@@ -3,14 +3,17 @@ import CommentSection from "../CommentSection/CommentSection";
 
 
 function PostContainer(props) {
-
-    console.log(props)
     return (
             <> 
 //             {props.postArray.map(postObject => (
                 // return(
                 <div className = "Post-object">
+                    <img src ={postObject.imageUrl} />
                     <CommentSection postComments = {postObject.comments} />
+                    <form>
+                        <input
+                            placeholder="comment"></input>
+                    </form>
                 </div>)
             )}
             </>
