20aced6644773d89774073facaf981f1bc63cfbb
justiny2c <justiny2c@gmail.com>
2019-06-06 13:45:05 -0700

    WIP: LoginPage, basic styled

--- instagram-app/src/components/Login/Login.js
+++ instagram-app/src/components/Login/Login.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import "./Login.css";
+import styled from 'styled-components';
 
 
 class Login extends React.Component {
@@ -32,28 +32,56 @@ class Login extends React.Component {
 
     render(){
         return (
-            <div className = "loginPage">
+            <LoginDiv className = "loginPage">
             <img
                 src = {require("./instagramLogInPhoto.png")}/>
-            <form>
-                <input
+            <Form>
+                <Input
                     placeholder= "Username"
                     onChange={this.handleChangeUsername}
                     value={this.state.username}
                     />
-                <input
+                <Input
                     placeholder= "Password"
                     onChange={this.handleChangePassword}
                     value={this.state.password}
                 
                     />
-                <button
-                    onClick={this.login}>Login</button>    
+                <Button
+                    onClick={this.login}>Login</Button>    
 
-            </form>
-            </div>
+            </Form>
+            </LoginDiv>
         )
     }
 }
+
+const LoginDiv = styled.div `
+    display: flex;
+    flex-flow: row;
+    // border: 2px solid black;
+    // background: blue;
+    align-items: center;
+    justify-content: center;
+`
+
+const Form = styled.form `
+    margin: 0;
+    padding: 0;
+`
+
+const Input = styled.input `
+    margin-bottom: 10px;
+    padding: 10px;
+    background: #f2f2f2;
+    `
+
+const Button = styled.button `
+    padding: 5px 10px;
+    margin-top: 10px;
+    border-radius: 5px;
+    font-family: 'proxima-nova', sans-serif;
+    font-weight: bold;
+`
 
 export default Login;
